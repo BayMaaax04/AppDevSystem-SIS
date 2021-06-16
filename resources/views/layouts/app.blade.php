eb141e222584a905606cfe8200c47b0fda896968
Jan Mikko Malizon <malizonjanmikko@gmail.com>
2021-06-17 03:52:05 +0800

    Update dark mode icon
    updated toggle dark mode con

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" x-data="{ dark: true }">
 
 <head>
     <meta charset="utf-8">
@@ -21,6 +21,7 @@
       } else if (localStorage.theme === 'dark') {
           document.querySelector('html').classList.add('dark')
       }
+
     </script>
 
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -38,7 +39,7 @@
 
 </head>
 
-<body class="bg-gray-100 dark:bg-gray-primary-dark antialiased leading-none font-sans">
+<body class="bg-gray-100 dark:bg-gray-primary-dark antialiased leading-none font-sans" >
     <div id="app">
       
         <header class="dark:bg-gray-secondary-dark bg-white lg:px-20 px-6 flex flex-wrap items-center lg:py-1 py-2 sticky">
@@ -82,8 +83,27 @@
               </div>
 
               <div class="lg:relative absolute lg:right-0 lg:top-0 right-2 top-2 flex-end flex justify-end">
-                <button id="switchTheme" class="dark:text-white h-10 w-10 flex justify-center items-center focus:outline-none text-black-500 mx-2">
-                  <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"></path></svg>
+                
+                <button @click="dark = !dark" id="switchTheme" class="group p-2 transition-colors duration-200 rounded-xl bg-blue-200 hover:bg-blue-200 dark:active:bg-gray-200 active:bg-gray-200  focus:outline-none mr-2">
+                  <svg
+                    x-show="dark"
+                    width="24"
+                    height="24"
+                    class="fill-current dark:text-gray-50 transform -rotate-90 "
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke=""
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
+                    />
+                  </svg>
+
+                  <svg x-bind:class="{ 'hidden': dark }" role="presentation" aria-hidden="true" class="w-6 h-6 fill-current text-white dark:active:text-gray-700" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"></path></svg>
                 </button>
 
                 <div x-data="{ dropdownOpen: false }" class="lg:relative">
@@ -132,6 +152,8 @@
 </body>
 {{-- @push('scripts') --}}
 <script>
+
+
     document.getElementById("switchTheme").addEventListener("click", function() {
         let htmlClasses = document.querySelector("html").classList;
         if (localStorage.theme == "dark") {
